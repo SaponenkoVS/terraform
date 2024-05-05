4d306bdea386c518baf85d72f118a22c3c7aeb6e
SaponenkoVS <saponenkovladislav2001@mail.ru>
2024-05-05 13:55:32 +0200

    init

--- test-terraform-workflow/main.tf
+++ test-terraform-workflow/main.tf
@@ -1,3 +1,9 @@
+terraform {
+  backend "local" {
+    path = "./terraform.tfstate"
+  }
+}
+
 resource "local_file" "script" {
   content  = var.console_log_variable
   filename = "${path.module}/script.js"
